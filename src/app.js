f8e07d56026ba12f34651bffe8a474c633b581c2
Jack13-Han <hanwaihtun1e.com>
2025-10-03 16:01:42 +0900

    add delete proprity

--- src/app.js
+++ src/app.js
@@ -1,40 +1,97 @@
 const addListBtn = document.querySelector("#btn");
 const textInput = document.querySelector("#textInput");
 const title = document.querySelector("#title");
+const taskTotal =document.querySelector("#taskTotal")
+const doneTaskTotal =document.querySelector("#doneTaskTotal")
+
+
+const editBtn =document.querySelector("#edit")
 
 const listGroup =document.querySelector("#listGroup")
 
 //process
 
 const addList = () => {
-  title.innerHTML = textInput.value;
+//   title.innerHTML = textInput.value;
+  listGroup.append(createNewList(textInput.value))
+
   textInput.value = null;
+    updateTaskTotal();
+
 };
+
+const updateTaskTotal=() => {
+      //count List
+  const list = document.querySelectorAll(".list");
+    taskTotal.innerHTML=list.length;
+}
+
+const updateDoneTaskTotal=() => {
+      //count List and update
+  const list = document.querySelectorAll(".list input:checked");
+    doneTaskTotal.innerHTML=list.length;
+}
 
 //create Element List
 
-const createNewList =() => {
+const createNewList =(currentTask) => {
     
 const list = document.createElement("div");
 
 list.classList.add("list");
 
+
+
 list.innerHTML = `
 <div class="px-3 mb-3 border border-2 py-2 flex flex-wrap justify-around items-center ">
-    <input class="" type="checkbox">
-    <p>
-        san kyi tr pr
+    <input class="list-done-check accent-[#3f3b3b]" type="checkbox">
+    <p class="font-serif list-task">
+        ${currentTask}
     </p>
 
     <div class="control">
-        <button class="bg-[#f4fa9c] px-4 py-2 rounded-xl"><a href="#"><i
+        <button  class="list-edit bg-[#f4fa9c] px-4 py-2 rounded-xl"><a href="#"><i
                     class="fa-solid fa-pencil"></i></a></button>
-        <button class="bg-[#ff304f] px-4 py-2 rounded-xl"><a href="#"><i
+
+        <button  class="list-delete bg-[#ff304f] px-4 py-2 rounded-xl"><a href="#"><i
                     class="fa-solid fa-trash"></i></a></button>
     </div>
 </div>
 `;
 
+const listDone=list.querySelector(".list-done-check")
+const listThrough=list.querySelector(".list-task")
+
+const listDeleteBtn=list.querySelector(".list-delete")
+const listEditBtn =list.querySelector(".list-edit")
+
+
+
+
+listDone.addEventListener("change",() => {
+    // console.log(currentTask+"is done")    
+    updateDoneTaskTotal();
+    listThrough.classList.toggle("line-through")
+    list.classList.toggle("opacity-20")
+    list.classList.toggle("scale-90")
+    list.classList.add("duration-200")
+
+
+})
+
+listDeleteBtn.addEventListener("click",() => {
+    if(window.confirm("Are you sure to delete..?")){
+        list.remove();
+    }
+})
+
+listEditBtn.addEventListener("click",() => {
+    console.log("edit")
+    const newTaskInput =document.createElement("input")
+    newTaskInput.className="border border-2 px-3 focus-visible:outline-none"
+    list.after(newTaskInput)
+    newTaskInput.value=currentTask;
+})
 return list;
 }
 
@@ -50,9 +107,9 @@ return list;
 
 //mount list to listGroup
 
-listGroup.append(createNewList())
 
 //event
 
 addListBtn.addEventListener("click", addList);
+// editBtn.addEventListener("click",edit);
 
